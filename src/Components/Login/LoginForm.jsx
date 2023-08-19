6eb68e3650ab32f5d1bbf70f49e8bf3b8beab3ad
Rafael Cunha <rafael.bispocunha@outlook.com>
2023-08-19 12:05:32 -0300

    Form Component

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,49 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Input from "../Forms/Input";
+import Button from "../Forms/Button";
 
 const LoginForm = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const [username, setUsername] = React.useState('')
-    const [password, setPassword] = React.useState('')
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token',{
-            method: 'POST',
-            headers:{
-                'Content-type': 'application/json',
-            },
-            body: JSON.stringify({username, password}),
-        }).then(response =>{
-            console.log(response)
-            return response;
-        }).then(json =>{
-            console.log(json)
-        })
-    }
-
-    return (
+  return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <input 
-            type="text" 
-            value={username}
-            onChange={({target}) => setUsername(target.value)}
-        />
-        <input 
-            type="password" 
-            value={password}
-            onChange={({target}) => setPassword(target.value)}
-        />
-        <button>Entrar</button>
+        <Input label="Usuário" type="text" name="username"/>
+        <Input label="Senha" type="password" name="password" />
+        <Button>Entrar</Button>
       </form>
-      <Link to="/login/criar">
-        Cadastro
-      </Link>
+      <Link to="/login/criar">Cadastro</Link>
     </section>
-    )
-  };
-  
-  export default LoginForm;
-  
+  );
+};
+
+export default LoginForm;
